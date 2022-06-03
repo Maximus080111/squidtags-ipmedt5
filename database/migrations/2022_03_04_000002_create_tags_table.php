efd8c29d2c8e82d05e160b6dc148c26861fc32e3
gerrietech <91469237+gerrietech@users.noreply.github.com>
2022-06-03 10:30:41 +0200

    Update and rename 2019_12_14_000001_create_personal_access_tokens_table.php to 2022_03_04_000002_create_tags_table.php
    Database migration added

---
+++ database/migrations/2022_03_04_000002_create_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('TagData')->unique();
+            $table->string('TagName');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
